3826f5473ad03da84244ac012b7ab3b842c0818d
JohnnyOzark <jrogerowen@protonmail.com>
2026-02-11 09:07:40 -0800

    updated .gitignore. created Logger.py to log performance. Created constants.py with screen dimensions. updated main.py: clock and black screen

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    dt = 0
+
+    while True:
+        log_state()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+                
+        
+        dt = clock.tick(60) / 1000
+    
     print(f"Starting Asteroids with pygame version:{pygame.version.ver}")
+    print("Screen width: 1280")
+    print("Screen height: 720")
 
 
 if __name__ == "__main__":
