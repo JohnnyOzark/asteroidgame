a46b2fd946acdecb32aa0b5c3059b0346bb4dbf6
JohnnyOzark <jrogerowen@protonmail.com>
2026-02-11 11:01:00 -0800

    created circleshape.py w/ circle class. created player.py for ship code inherited from CircleShape. updated constants player info. updated main with player

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    ship = Player(x = (SCREEN_WIDTH / 2), y = (SCREEN_HEIGHT / 2))
     clock = pygame.time.Clock()
     dt = 0
+
+    print(f"Starting Asteroids with pygame version:{pygame.version.ver}")
 
     while True:
         log_state()
@@ -16,15 +19,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        ship.draw(screen)
         pygame.display.flip()
                 
         
         dt = clock.tick(60) / 1000
     
-    print(f"Starting Asteroids with pygame version:{pygame.version.ver}")
-    print("Screen width: 1280")
-    print("Screen height: 720")
-
 
 if __name__ == "__main__":
     main()
